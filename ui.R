41b24a22d6bfd64f936a0dabe5651990be0b41dc
Sophie Riches <sophie.riches@education.gov.uk>
2024-05-10 09:45:45 +0100

    Removed reference to Site 2 at line 122

--- ui.R
+++ ui.R
@@ -121,8 +121,7 @@ ui <- function(input, output, session) {
         "In case of slowdown or connection issues due to high demand, we have
         produced two instances of this site which can be accessed at the
         following links: ",
-        "<a href=", site_primary, " id='link_site_1'>Site 1</a> and ",
-        "<a href=", site_overflow, " id='link_site_2'>Site 2</a>."
+        "<a href=", site_primary, " id='link_site_1'>Site 1</a>"
       )
     ),
     shiny::navlistPanel(
